84e00323673b72ca1ba99ad5ed90cdd6f11c0688
Prince <132038268+PRINCE3719@users.noreply.github.com>
2023-09-06 07:30:22 -0700

    Add files via upload

---
+++ mocks/sample.js
@@ -0,0 +1,9 @@
+function drop(){
+    const buut = document.getElementById("drop-butn");
+    const droo = document.getElementById("myDropdown");
+    droo.style.display = "block";
+}
+function dropof(){
+    const droo = document.getElementById("myDropdown");
+    droo.style.display = "none";
+}
